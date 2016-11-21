50a036972755b6f8cfc9067e4033ddc53484c972
megan liken <meganliken@megans-MacBook-Pro-2.local>
2016-11-20 22:27:18 -0600

    game complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,21 @@
  $(document).ready(function() {
 var questions =[
         {
+        question: "Name the primary red grape used in Argentina",
+        answer: 4,
+        choices: {
+            "1": "Pinot Noir",
+            "2": "Burgundy",
+            "3": "Syrah", 
+            "4": "Malbec", //correct answer
+            }
+        },             
+        {
         question: "What is Cenosillicaphobia the fear of?",
         answer: 2,
         choices: {
             "1": "Fear of foam", 
-            "2": "Fear of an empty glass", //correct answer
+            "2": "Fear of an empty glass", 
             "3": "Fear of carbonation",
             "4": "Fear of a spilling"
             }
@@ -41,16 +51,7 @@ var questions =[
             "4": "Argentina"
             }
         },       
-         {
-        question: "Name the primary red grape used in Argentina",
-        answer: 4,
-        choices: {
-            "1": "Pinot Noir",
-            "2": "Burgundy",
-            "3": "Syrah", 
-            "4": "Malbec", //correct answer
-            }
-        },             
+         
          {
         question: "What is fermentation?",
         answer: 1,
@@ -110,4 +111,97 @@ var questions =[
     }
     startGame();
 
-    
+    //displays questions after each is answered or time runs out
+    function displayQuestions(){
+        //restarts countdown timer at each question
+        clearTimeout(next);
+        stopCounter();
+        counter();
+
+        $("#question").html("<h2>"+questions[i].question+ "</h2");
+        $("#answer1").text(questions[i].choices[1]);
+        $("#answer2").text(questions[i].choices[2]);
+        $("#answer3").text(questions[i].choices[3]);
+        $("#answer4").text(questions[i].choices[4]);
+    }
+    //moves to next array element: next question
+    function nextQuestion(){
+        i++;
+        if(i == questions.length){
+            return gameEnd();
+        }
+        stopCounter();
+        //timeout for displaying answers
+        var next = setTimeout(function(){ displayQuestions() },3000);
+    }
+    //click listener for each answer
+    function userAnswer(id){
+        $(id).on('click',function(){
+            clearInterval(timer);
+            userAnswer = $(this).text();
+            //variable contains the number of correct answer
+            var answerNumber = questions[i].answer;
+            //variable contains the string of correct answer
+            var answerString = questions[i].choices[answerNumber]
+            //checks answers if right or wrong
+            if(userAnswer == answerString){
+                correct++;
+             //displays incorrect   
+            } else{
+                incorrect++;
+                
+            }
+            nextQuestion();
+        });
+    }
+    //time countdown, checks if time runs out
+    var countDown = function(){
+        time--;
+        console.log("i variable: "+i)
+        console.log("time variable: "+time);
+        $('#timer').text(time);
+        if(time == 0){
+            stopCounter();
+            nextQuestion();
+            showAnswers();
+        }
+    }
+    //timer for each question: 15 seconds
+    var counter = function(){
+        clearInterval(timer);
+        time = 15;
+        timer = setInterval(countDown,1000);
+    }
+    //stops timer
+    function stopCounter(){
+        clearInterval(timer);
+    }
+    //displays game score
+    var gameEnd = function(){
+        stopCounter();
+        $(".triviaQuestions").addClass("noShow");
+        $('#correct').text(correct);
+        $('#incorrect').text(incorrect);
+        $('#finalScore').removeClass("noShow");
+    }
+    //click to restart game
+    function restart(){
+        $('#reset').on('click', function(){
+            reset();
+            $('.triviaQuestions').removeClass('noShow')
+            displayQuestions();
+            $("#start").addClass('noShow');
+            $("#finalScore").addClass('noShow');
+        });
+    }
+    restart();
+    //resets variables
+    var reset = function(){
+        i = 0;
+        correct = 0;
+        incorrect =0;
+    }
+});
+
+       
+        
